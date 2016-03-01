25d1296aa2ed29c7fd305ffcab232fccbeed42de
handy <jxndwyr@163.com>
2016-03-01 10:11:13 +0800

    框架规划构建

--- gulpfile.js
+++ gulpfile.js
@@ -61,7 +61,9 @@ var PATH = {
 gulp.task('styles', function() {
     //sass
     gulp.src([PATH.src.sass + '/*.scss','!' + PATH.src.sass + '/modules/**/*.scss'])
-        .pipe(sass().on('error', sass.logError))
+    //Type: String Default: nested Values: nested, expanded, compact, compressed
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        //.pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(gulp.dest(PATH.dest.css))
         .pipe(rename({ suffix: '.min' }))
